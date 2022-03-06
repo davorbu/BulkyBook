6363347998dd4ec13b011358a6341ee94ff991c0
Davor Burmudžija <davorburmudzija@gmail.com>
2022-03-06 20:10:03 +0100

    Add project files.

---
+++ BulkyBook.DataAccess/Repository/OrderDetailRepository.cs
@@ -0,0 +1,25 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class OrderDetailRepository : Repository<OrderDetail>, IOrderDetailRepository
+    {
+        private ApplicationDbContext _db;
+
+        public OrderDetailRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();  
+        }
+
+        public void Update(OrderDetail obj)
+        {
+            _db.OrderDetail.Update(obj);
+        }
+    }
+}
